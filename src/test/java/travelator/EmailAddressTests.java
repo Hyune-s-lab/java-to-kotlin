55b384d160aa449def58120cdb72797f72cac55d
Hyune-c <choi8608@gmail.com>
2022-11-30 21:07:31 +0900

    3.7

--- src/test/java/travelator/EmailAddressTests.java
+++ src/test/java/travelator/EmailAddressTests.java
@@ -10,30 +10,30 @@ class EmailAddressTests {
     @Test
     void parsing() {
         assertEquals(
-            new EmailAddress("fred", "example.com"), EmailAddress.Companion.parse("fred@example.com")
+            new EmailAddress("fred", "example.com"), EmailAddress.parse("fred@example.com")
         );
     }
 
     @Test
     void parsingFailures() {
         assertThrows(
-            IllegalArgumentException.class, () -> EmailAddress.Companion.parse("@")
+            IllegalArgumentException.class, () -> EmailAddress.parse("@")
         );
         assertThrows(
-            IllegalArgumentException.class, () -> EmailAddress.Companion.parse("fred@")
+            IllegalArgumentException.class, () -> EmailAddress.parse("fred@")
         );
         assertThrows(
-            IllegalArgumentException.class, () -> EmailAddress.Companion.parse("@example.com")
+            IllegalArgumentException.class, () -> EmailAddress.parse("@example.com")
         );
         assertThrows(
-            IllegalArgumentException.class, () -> EmailAddress.Companion.parse("fred_at_example.com")
+            IllegalArgumentException.class, () -> EmailAddress.parse("fred_at_example.com")
         );
     }
 
     @Test
     void parsingWithAtInLocalPart() {
         assertEquals(
-            new EmailAddress("\"b@t\"", "example.com"), EmailAddress.Companion.parse("\"b@t\"@example.com")
+            new EmailAddress("\"b@t\"", "example.com"), EmailAddress.parse("\"b@t\"@example.com")
         );
     }
 }
