1900ea491d5739af04345dfa458a4107a57524aa
Hyune-c <choi8608@gmail.com>
2022-12-02 00:17:11 +0900

    7.6

---
+++ src/main/java/travelator/ITrackTrips.java
@@ -0,0 +1,8 @@
+package travelator;
+
+import java.util.Optional;
+
+public interface ITrackTrips {
+
+    Optional<Trip> currentTripFor(String customerId);
+}
---
+++ src/main/java/travelator/Trips.java
@@ -0,0 +1,9 @@
+package travelator;
+
+import java.util.Set;
+
+public interface Trips {
+    Set<Trip> tripsFor(String customerId);
+
+    Set<Trip> currentTripsFor(String customerId);
+}
