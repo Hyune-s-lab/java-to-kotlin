0c831da8ce92cedc7ccab6e9eb0fcc45a3a2ca10
Hyune-c <choi8608@gmail.com>
2022-12-03 00:24:51 +0900

    9.23

--- src/main/java/travelator/EmailAddress.kt
+++ src/main/java/travelator/EmailAddress.kt
@@ -9,14 +9,19 @@ data class EmailAddress(
 
     companion object {
         @JvmStatic
-        fun parse(value: String): EmailAddress = value.lastIndexOf('@').let { atIndex ->
-            require(!(atIndex < 1 || atIndex == value.length - 1)) {
-                "EmailAddress must be two parts separated by @"
+        fun parse(value: String): EmailAddress =
+            value.split('@').let { (leftPart, rightPart) ->
+                EmailAddress(leftPart, rightPart)
             }
-            EmailAddress(
-                value.substring(0, atIndex),
-                value.substring(atIndex + 1)
-            )
-        }
     }
+}
+
+private fun String.split(divider: Char): Pair<String, String> {
+    val atIndex = lastIndexOf(divider)
+    require(!(atIndex < 1 || atIndex == length - 1)) {
+        "EmailAddress must be two parts separated by @"
+    }
+    val leftPart = substring(0, atIndex)
+    val rightPart = substring(atIndex + 1)
+    return Pair(leftPart, rightPart)
 }
