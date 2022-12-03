b2dddf17f4a3c37c8f9626a14f1e465fc80c964d
Hyune-c <choi8608@gmail.com>
2022-12-04 03:03:33 +0900

    11.12

--- src/main/java/travelator/CampSites.kt
+++ src/main/java/travelator/CampSites.kt
@@ -1,13 +1,13 @@
 package travelator
 
-import java.util.stream.Collectors
-
-fun Set<CampSite>.sitesInRegion(
+fun Iterable<CampSite>.sitesInRegion(
     countryISO: String,
     region: String
-): Set<CampSite> = stream()
-    .filter { campSite: CampSite ->
-        campSite.countryCode == countryISO &&
-                campSite.region.equals(region, ignoreCase = true)
-    }
-    .collect(Collectors.toUnmodifiableSet())
+): Set<CampSite> =
+    filter { site ->
+        site.isIn(countryISO, region)
+    }.toSet()
+
+fun CampSite.isIn(countryISO: String, region: String? = null) =
+    countryCode == countryISO &&
+            region?.equals(this.region, ignoreCase = true) ?: true
