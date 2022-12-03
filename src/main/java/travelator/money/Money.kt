2635c1b31acf08e8f7252f61d338694d49fcbfe0
Hyune-c <choi8608@gmail.com>
2022-12-04 05:17:56 +0900

    12.5

--- src/main/java/travelator/money/Money.kt
+++ src/main/java/travelator/money/Money.kt
@@ -3,51 +3,50 @@ package travelator.money
 import java.math.BigDecimal
 import java.util.*
 
-data class Money  // <1>
-private constructor(// <2>
-    val amount: BigDecimal, // <3>
+class Money private constructor(
+    val amount: BigDecimal,
     val currency: Currency
 ) {
-    override fun equals(o: Any?): Boolean { // <3>
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val money = o as Money
-        return amount == money.amount && currency == money.currency
+    override fun equals(other: Any?) =
+        this === other ||
+                other is Money &&
+                amount == other.amount &&
+                currency == other.currency
+
+    override fun hashCode() =
+        Objects.hash(amount, currency)
+
+    override fun toString() =
+        amount.toString() + " " + currency.currencyCode
+
+    fun add(that: Money): Money {
+        return plus(that)
     }
 
-    override fun hashCode(): Int { // <3>
-        return Objects.hash(amount, currency)
-    }
-
-    override fun toString(): String { // <4>
-        return amount.toString() + " " + currency.currencyCode
-    }
-
-    @JvmName("add")
     operator fun plus(that: Money): Money {
-        require(currency == that.currency) { "cannot add Money values of different currencies" }
+        require(currency == that.currency) {
+            "cannot add Money values of different currencies"
+        }
         return Money(amount.add(that.amount), currency)
     }
 
     companion object {
         @JvmStatic
-        fun of(amount: BigDecimal, currency: Currency): Money { // <1>
-            return Money(
-                amount.setScale(currency.defaultFractionDigits),
-                currency
-            )
-        }
+        fun of(amount: BigDecimal, currency: Currency) = Money(
+            amount.setScale(currency.defaultFractionDigits),
+            currency
+        )
 
-        fun of(amountStr: String?, currency: Currency): Money { // <2>
-            return of(BigDecimal(amountStr), currency)
-        }
+        @JvmStatic
+        fun of(amountStr: String, currency: Currency) =
+            of(BigDecimal(amountStr), currency)
 
-        fun of(amount: Int, currency: Currency): Money {
-            return of(BigDecimal(amount), currency)
-        }
+        @JvmStatic
+        fun of(amount: Int, currency: Currency) =
+            of(BigDecimal(amount), currency)
 
-        fun zero(userCurrency: Currency): Money {
-            return of(BigDecimal.ZERO, userCurrency)
-        }
+        @JvmStatic
+        fun zero(userCurrency: Currency) =
+            of(BigDecimal.ZERO, userCurrency)
     }
 }
