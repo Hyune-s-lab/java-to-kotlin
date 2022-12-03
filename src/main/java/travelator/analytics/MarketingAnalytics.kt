6c3b586c3f7f5a9ab6f001250eb2f5cf01f5f6ba
Hyune-c <choi8608@gmail.com>
2022-12-04 07:12:05 +0900

    13.28

--- src/main/java/travelator/analytics/MarketingAnalytics.kt
+++ src/main/java/travelator/analytics/MarketingAnalytics.kt
@@ -6,17 +6,20 @@ class MarketingAnalytics(
     private val eventStore: EventStore
 ) {
     fun averageNumberOfEventsPerCompletedBooking(timeRange: String): Double {
-        return eventStore
-            .queryAsSequence("type=CompletedBooking&timerange=$timeRange")
-            .flatMap { event ->
-                val interactionId = event["interactionId"] as String
-                eventStore
-                    .queryAsSequence("interactionId=$interactionId")
-            }
+        return eventStore.queryAsSequence("type=CompletedBooking&timerange=$timeRange")
+            .allEventInSameInteractions()
             .groupBy { event ->
                 event["interactionId"] as String
-            }.values.averageBy { it.size }
+            }.values
+            .averageBy { it.size }
     }
+
+    private fun Sequence<MutableMap<String, Any>>.allEventInSameInteractions() =
+        flatMap { event ->
+            val interactionId = event["interactionId"] as String
+            eventStore
+                .queryAsSequence("interactionId=$interactionId")
+        }
 
     private fun <T> Collection<T>.averageBy(selector: (T) -> Int) =
         sumOf(selector) / size.toDouble()
