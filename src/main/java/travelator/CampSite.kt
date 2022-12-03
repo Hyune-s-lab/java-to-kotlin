83a1c1ebb967223b71ded4f7ca45ba82acc3293e
Hyune-c <choi8608@gmail.com>
2022-12-04 02:54:51 +0900

    11.9

---
+++ src/main/java/travelator/CampSite.kt
@@ -0,0 +1,14 @@
+package travelator
+
+class CampSite(
+    val id: String,
+    val name: String,
+    val address: Address
+) {
+
+    val countryCode: String
+        get() = address.countryCode
+
+    val region: String
+        get() = address.region
+}
---
+++ src/main/java/travelator/CampSites.kt
@@ -0,0 +1,13 @@
+package travelator
+
+import java.util.stream.Collectors
+
+fun Set<CampSite>.sitesInRegion(
+    countryISO: String,
+    region: String
+): Set<CampSite> = stream()
+    .filter { campSite: CampSite ->
+        campSite.countryCode == countryISO &&
+                campSite.region.equals(region, ignoreCase = true)
+    }
+    .collect(Collectors.toUnmodifiableSet())
