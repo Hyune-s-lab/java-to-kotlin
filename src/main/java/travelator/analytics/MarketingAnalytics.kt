18d865193a55b495a6a7efcd23a2f033fc474735
Hyune-c <choi8608@gmail.com>
2022-12-04 06:45:16 +0900

    13.3

---
+++ src/main/java/travelator/analytics/MarketingAnalytics.kt
@@ -0,0 +1,26 @@
+package travelator.analytics
+
+import java.util.function.Function
+import java.util.stream.Collectors
+
+class MarketingAnalytics(
+    private val eventStore: EventStore
+) {
+    fun averageNumberOfEventsPerCompletedBooking(timeRange: String): Double {
+        val eventsForSuccessfulBookings = eventStore
+            .queryAsStream("type=CompletedBooking&timerange=$timeRange")
+            .flatMap { event: Map<String?, Any?> ->
+                val interactionId = event["interactionId"] as String?
+                eventStore.queryAsStream("interactionId=$interactionId")
+            }
+        val bookingEventsByInteractionId = eventsForSuccessfulBookings.collect(
+            Collectors.groupingBy(Function { event: Map<String, Any> -> event["interactionId"] as String? })
+        )
+        val averageNumberOfEventsPerCompletedBooking = bookingEventsByInteractionId
+            .values
+            .stream()
+            .mapToInt { obj: List<Map<String, Any>> -> obj.size }
+            .average()
+        return averageNumberOfEventsPerCompletedBooking.orElse(Double.NaN)
+    }
+}
