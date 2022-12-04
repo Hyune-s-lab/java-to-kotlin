cddc2c91f398e4cfd6a17aed9cce10ad66d839a7
Hyune-c <choi8608@gmail.com>
2022-12-05 03:38:05 +0900

    15.38

--- src/main/java/travelator/itinerary/Itinerary.kt
+++ src/main/java/travelator/itinerary/Itinerary.kt
@@ -1,8 +1,15 @@
 package travelator.itinerary
 
 import travelator.Id
+import java.time.Duration
 
-class Itinerary(
+data class Itinerary(
     val id: Id<Itinerary>,
     val route: Route
-)
+) : Route by route {
+    fun withTransformedRoute(transform: (Route).() -> Route) =
+        copy(route = transform(route))
+}
+
+fun Route.hasJourneyLongerThan(duration: Duration) =
+    any { it.duration > duration }
