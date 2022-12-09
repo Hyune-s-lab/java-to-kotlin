516ad1f1fae2cb39389e193bb4a2a5de5281e8ab
Hyune-c <choi8608@gmail.com>
2022-12-10 03:48:00 +0900

    19.14

---
+++ src/main/java/travelator/Customers.kt
@@ -0,0 +1,20 @@
+package travelator
+
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Success
+import java.util.*
+
+interface Customers {
+    @Throws(DuplicateException::class)
+    fun add(name: String, email: String): Customer
+
+    fun addToo(name: String, email: String)
+            : Result<Customer, DuplicateException> =
+        try {
+            Success(add(name, email))
+        } catch (x: DuplicateException) {
+            Failure(x)
+        }
+
+    fun find(id: String): Optional<Customer>
+}
