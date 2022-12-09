d1d84b43b69d565947aaa4c087f149ce880b80cc
Hyune-c <choi8608@gmail.com>
2022-12-09 20:11:45 +0900

    18.15

--- src/main/java/travelator/itinerary/ItineraryItem.kt
+++ src/main/java/travelator/itinerary/ItineraryItem.kt
@@ -1,12 +1,49 @@
 package travelator.itinerary
 
 import travelator.Id
-import travelator.geo.MapOverlay
+import travelator.Location
+import travelator.geo.Position
 import travelator.money.Money
+import java.time.Period
+import java.time.ZonedDateTime
 
-interface ItineraryItem {
-    val id: Id<ItineraryItem>
-    val description: String
-    val costs: List<Money>
-    val mapOverlay: MapOverlay
+sealed class ItineraryItem { // <1>
+    abstract val id: Id<ItineraryItem> // <2>
 }
+
+data class Accommodation(
+    override val id: Id<Accommodation>,
+    val location: Location,
+    val checkInFrom: ZonedDateTime,
+    val checkOutBefore: ZonedDateTime,
+    val pricePerNight: Money
+) : ItineraryItem() { // <3>
+    val nights = Period.between(
+        checkInFrom.toLocalDate(),
+        checkOutBefore.toLocalDate()
+    ).days
+    val totalPrice: Money = pricePerNight * nights
+}
+
+data class Attraction(
+    override val id: Id<Attraction>,
+    val location: Location,
+    val notes: String
+) : ItineraryItem()
+
+data class Journey(
+    override val id: Id<Journey>,
+    val travelMethod: TravelMethod,
+    val departsFrom: Location,
+    val departureTime: ZonedDateTime,
+    val arrivesAt: Location,
+    val arrivalTime: ZonedDateTime,
+    val price: Money,
+    val path: List<Position> = listOf(departsFrom.position, arrivesAt.position),
+) : ItineraryItem()
+
+data class RestaurantBooking(
+    override val id: Id<RestaurantBooking>,
+    val location: Location,
+    val time: ZonedDateTime
+) : ItineraryItem()
